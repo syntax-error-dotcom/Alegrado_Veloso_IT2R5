62d026da664f2bf187180158dbeda9588f1a6eca
Carlo Emmanuel Veloso <veloso.carloemmanuel215@gmail.com>
2026-04-05 19:05:13 +0800

    logout button works

--- app/controllers/adminController.php
+++ app/controllers/adminController.php
@@ -3,18 +3,19 @@ session_start();
 $root = dirname(__DIR__);
 include($root . '/config/config.php');
 
-if (isset($POST['logoutButton'])) {
+
+if (isset($_POST['logoutButton'])) {
     unset($_SESSION['authUser']);
     unset($_SESSION['user_id']);
     unset($_SESSION['role']);
     session_unset();
     session_destroy();
 
-    header("Location: /eLibrary/public/login");
+    header("Location: /eLibrary/public/login.php");
     exit();
 
 
+} else {
+    exit();
 }
 
-
-?>
--- public/admin/index.php
+++ public/admin/index.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
 include('../../app/middleware/admin.php');
 include('./includes/header.php');
 include('./includes/sidebar.php');
