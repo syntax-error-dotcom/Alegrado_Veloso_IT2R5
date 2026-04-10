331e8741787913ac9725d0342efde278c870e49e
Anthony955-tech <alegradojamesanthony@gmail.com>
2026-04-11 00:30:03 +0800

    implementing my changes

---
+++ public/user/accountability.php
@@ -0,0 +1,16 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
+include('../../app/middleware/user.php');
+include(__DIR__ . '/includes/header.php');
+include(__DIR__ . '/includes/sidebar.php');
+include(__DIR__ . '/includes/topbar.php');
+?>
+
+
+<h1>accountability PAGE</h1>
+
+<?php
+include(__DIR__ . '/includes/footer.php');
+?>
