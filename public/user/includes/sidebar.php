ff0bc3c949b465b544621de9e4eeb63518302d0a
Anthony955-tech <alegradojamesanthony@gmail.com>
2026-04-11 00:26:16 +0800

    includes

---
+++ public/user/includes/sidebar.php
@@ -0,0 +1,62 @@
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="./index.php">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="./index.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            
+             <li class="nav-item">
+                <a class="nav-link" href="./discovery.php">
+                    <i class="fas fa-fw fa-book-open"></i>
+                    <span>Discovery</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="./accountability.php">
+                    <i class="fas fa-fw fa-sync"></i>
+                    <span>Accountability</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="./feedback.php">
+                    <i class="fas fa-fw fa-sync"></i>
+                    <span>Feedback</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+         
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
