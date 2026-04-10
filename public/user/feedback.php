6ec3bc51c5f82b612539509e6b5863a7d0717e63
Anthony955-tech <alegradojamesanthony@gmail.com>
2026-04-11 00:51:26 +0800

    removing middle ware

--- public/user/feedback.php
+++ public/user/feedback.php
@@ -2,7 +2,6 @@
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
-include('../../app/middleware/user.php');
 include(__DIR__ . '/includes/header.php');
 include(__DIR__ . '/includes/sidebar.php');
 include(__DIR__ . '/includes/topbar.php');
